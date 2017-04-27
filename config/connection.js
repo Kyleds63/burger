bf589873b6f174fb5ed446776d90fe7d76ffd791
kyleds63 <kstoner63@gmail.com>
2017-04-26 20:29:21 -0500

    Stopped at config setup, step 3, bullet two

---
+++ config/connection.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+var port = 3000;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(__dirname + "/public"));
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307,
+  user: "root",
+  password: "root",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+
+});
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('In N Out',true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Five Guys',true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('P. Terrys',true);
